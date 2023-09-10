b765b46c44dd2f328c5cc6822160ecb871736215
smaizeal25 <samizeal25@gmail.com>
2023-09-10 16:54:39 +0100

    previews screen

--- lib/preview.dart
+++ lib/preview.dart
@@ -9,7 +9,9 @@ class Preview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: 
+      body: Container(
+        
+      );
     );
   }
 }
