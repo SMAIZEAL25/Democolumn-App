12e9441257c5d8cc804b50008322d6aa298a17fa
smaizeal25 <samizeal25@gmail.com>
2023-09-10 16:08:02 +0100

    preview

---
+++ lib/preview.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(const Preview());
+}
+
+class Preview extends StatelessWidget {
+  const Preview({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: 
+    );
+  }
+}
